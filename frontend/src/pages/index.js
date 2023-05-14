b0c900a29b6d91dec3c5c7184f32452e8311ee52
Ahmad Narendi <dewacheat55@gmail.com>
2023-05-14 19:05:45 +0700

    liat sendiri

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -4,29 +4,27 @@ import Jumbotron from "@/components/Jumbotron/Jumbotron";
 import InfoContact from "@/components/InfoContact/InfoContact";
 import Navbar from "@/components/layouts/Navbar";
 import Footer from "@/components/layouts/Footer";
+import Paskibra from "@/components/Extrakulikuler/Paskibra";
 
 const Home = () => {
   return (
     <>
-      {/* Navbar */}
-      <Navbar />
-      {/* End */}
-      {/* Jumbotron */}
-      <Jumbotron />
-      {/* Info Contact */}
-      <InfoContact />
-      
-      {/* End */}
-      {/* Headmaster */}
-      
+      <div className="bg-purple-950">
+        {/* Navbar */}
+        <Navbar />
+        <Paskibra />
+        {/* End */}
+        {/* Jumbotron */}
+        {/* <Jumbotron /> */}
+        {/* Info Contact */}
+        {/* <InfoContact /> */}
 
-      {/* End */}
-      {/* Jurusan */}
-      <MainContent />
-      {/* End */}
-      {/* Footer */}
-      <Footer />
-      {/* end */}
+        {/* End */}
+        {/* Jurusan */}
+        {/* <MainContent /> */}
+        {/* End */}
+        <Footer />
+      </div>
     </>
   );
 };
