6dc9e05b149dd2ce0331422545acde2e3e584dea
Ahmad Narendi <dewacheat55@gmail.com>
2023-05-14 14:08:24 +0700

    extra

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,17 +2,19 @@ import React from "react";
 import MainContent from "@/components/Cards/MainContent";
 import Jumbotron from "@/components/Jumbotron/Jumbotron";
 import InfoContact from "@/components/InfoContact/InfoContact";
+import Paskibra from "@/components/Paskibra/Paskibra";
+
 
 const Home = () => {
   return (
     <>
       {/* Navbar */}
-
+<Paskibra />
       {/* End */}
       {/* Jumbotron */}
-      <Jumbotron />
+      {/* <Jumbotron /> */}
       {/* Info Contact */}
-      <InfoContact />
+      {/* <InfoContact /> */}
       
       {/* End */}
       {/* Headmaster */}
@@ -20,7 +22,7 @@ const Home = () => {
 
       {/* End */}
       {/* Jurusan */}
-      <MainContent />
+      {/* <MainContent /> */}
       {/* End */}
     </>
   );
